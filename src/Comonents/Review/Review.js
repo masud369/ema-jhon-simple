9fc84613d4eefb8de4de89a89c57148880831156
masud369 <naushadalimasud999@gmail.com>
2022-09-07 20:26:19 +0600

    last commit

--- src/Comonents/Review/Review.js
+++ src/Comonents/Review/Review.js
@@ -1,61 +1,109 @@
-import React, { useEffect, useState } from 'react';
-import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/dataBaseManager';
-import fakeData from '../../fakeData/products.json';
-import Reveiwitem from '../Reviewitem/Reveiwitem';
-import Cart from '../Cart/Cart';
-import happyImage from '../../images/giphy.gif'
-
+import React, { useEffect, useState } from "react";
+import {
+  getDatabaseCart,
+  processOrder,
+  removeFromDatabaseCart,
+} from "../../utilities/dataBaseManager";
+import fakeData from "../../fakeData/products.json";
+import Reveiwitem from "../Reviewitem/Reveiwitem";
+import Cart from "../Cart/Cart";
+import happyImage from "../../images/giphy.gif";
 
 const Review = () => {
+  const [cart, setCart] = useState([]);
+  useEffect(() => {
+    const savCart = getDatabaseCart();
+    const prosuctKeys = Object.keys(savCart);
+    const cartProducts = prosuctKeys.map((id) => {
+      const product = fakeData.find((pd) => pd.id === id);
+      product.quantity = savCart[id];
+      return product;
+    });
+    setCart(cartProducts);
+  }, []);
 
-    const [ cart,setCart ]=useState([]);
-    useEffect(()=>{
-        
-       const savCart = getDatabaseCart();
-       const prosuctKeys = Object.keys(savCart)
-       const cartProducts = prosuctKeys.map(id=>{
-         const product = fakeData.find(pd=>pd.id===id);
-         product.quantity = savCart[id];
-         return product;
-         
-       });
-       setCart(cartProducts);
-       console.log(cartProducts);
-    },[])
+  // const total = cart.reduce( (total,pdr)=> total+pdr.price * pdr.quantity,0)
 
-    const removeItem = (idName)=>{
-      const newCart  = cart.filter(pd=>pd.id !== idName);
-      setCart(newCart);
-      removeFromDatabaseCart(idName);
+  // const removeQnty =(key)=>{
+
+  //     const newAdd =cart.find((pd) =>{
+  //      if(pd.id===key){
+  //       let result = pd.quantity=+1;
+  //       return result;
+  //      }
+  //     });
+
+  //      setCart(newAdd);
+  // }
+  //   // // const savCart = getDatabaseCart();
+  // // const prosuctKeys = Object.keys(savCart)
+  //   const product = fakeData.find(pd =>{ if(pd.id === itemId){
+  //   product.quantity =- 1;
+  //   }});
+  // setCart(product);
+  const addNmiuns = (itemId, option) => {
+    console.log(itemId);
+    const findItem = { ...cart.find((item) => item.id === itemId) };
+    if (findItem.quantity === 1 || findItem.quantity > 1) {
+      if (option === "+") {
+        findItem.quantity = findItem.quantity + 1;
+      } else if(findItem.quantity > 2 || findItem.quantity === 2 ){
+        findItem.quantity = findItem.quantity - 1;
+      }
+      let cartClone = [...cart];
+      let findIndex = cartClone.indexOf(
+        cart.find((item) => item.id === itemId)
+      );
+      cartClone[findIndex] = findItem;
+      setCart(cartClone);
     }
-    const [orderplaced, setOrderplaced] = useState(false);
-    const handelOrderPlace = ()=>{
-           setCart([]); 
-           setOrderplaced(true);
-           processOrder();
-          
-        }
-        let thanksImage;
-        if(orderplaced){
-          thanksImage = <img src={happyImage} alt="" />
-        }
-    return (
-        <div className="shop-container">
-          <div className='products-container'>
-           <h2> The total item {cart.length}</h2>
-          {
-            cart.map(pd=> <Reveiwitem removeItem={removeItem} product={pd} key={pd.id}></Reveiwitem> )
-          }
-           {thanksImage}
-        </div>
-        <div className="cart-container"> 
-                <Cart cart={cart}>
-                  <button onClick={handelOrderPlace} className='add-cart'>Place order</button>
-                </Cart>
-                
-            </div>
-        </div>
-    );
+  };
+
+  const removeQuantity = (itemId) => {
+    addNmiuns(itemId, "-");
+  };
+  const addQuantity = (itemId) => {
+    addNmiuns(itemId, "+");
+  };
+  const removeItem = (idName) => {
+    const newCart = cart.filter((pd) => pd.id !== idName);
+    setCart(newCart);
+    removeFromDatabaseCart(idName);
+  };
+  const [orderplaced, setOrderplaced] = useState(false);
+  const handelOrderPlace = () => {
+    setCart([]);
+    setOrderplaced(true);
+    processOrder();
+  };
+  let thanksImage;
+  if (orderplaced) {
+    thanksImage = <img src={happyImage} alt="" />;
+  }
+  return (
+    <div className="shop-container">
+      <div className="products-container">
+        <h2> The total item {cart.length}</h2>
+        {cart.map((pd) => (
+          <Reveiwitem
+            addQuantity={addQuantity}
+            removeQuantity={removeQuantity}
+            removeItem={removeItem}
+            product={pd}
+            key={pd.id}
+          ></Reveiwitem>
+        ))}
+        {thanksImage}
+      </div>
+      <div className="cart-container">
+        <Cart cart={cart}>
+          <button onClick={handelOrderPlace} className="add-cart">
+            Place order
+          </button>
+        </Cart>
+      </div>
+    </div>
+  );
 };
 
 export default Review;
--- src/Comonents/Reviewitem/Reveiwitem.js
+++ src/Comonents/Reviewitem/Reveiwitem.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Reveiwitem = (props) => {
-    const {name,quantity,id,price} = props.product;
-    const reviewItem = {
-        borderBottom:"1px solid red",
-        padding:'10px',
-    }
-    return (
-        <div style={reviewItem}>
-            <h4>Name:{' '+name}</h4> 
-            <p>Quantity:{' '+ quantity}</p> 
-            <p>Price:{' '+ price} </p>
-            <button 
-            onClick={()=>props.removeItem(id)}
-            className='add-cart'> Remove</button>
-        </div>
-    );
+  const { name, quantity, id, price, img } = props.product;
+  const reviewItem = {
+    borderBottom: "1px solid red",
+    padding: "10px",
+  };
+  return (
+    <div style={reviewItem}>
+      <img width={"120px"} src={img} alt="" />
+      <h4>Name:{" " + name}</h4>
+      <p>Quantity:{" " + quantity}</p>
+      <p>Price:{" " + price} </p>
+      <button onClick={() => props.removeItem(id)} className="add-cart">
+        {" "}
+        Remove
+      </button>
+      <button onClick={() => props.removeQuantity(id)}>removeQuantity</button>
+      <button onClick={() => props.addQuantity(id)}>add Quantity</button>
+    </div>
+  );
 };
 
 export default Reveiwitem;
